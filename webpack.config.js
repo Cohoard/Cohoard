f6c06eca1c9ced3d641ee4cd749f7724a304812d
dzidupe <ekdzidupe@gmail.com>
2022-07-29 17:56:26 -0400

    Boilerplate added

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, './build'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'build'),
+      publicPath: '/build',
+    },
+    // historyApiFallback: true,
+    proxy: {
+      '/**': 'http://localhost:3000',
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: './client/index.html',
+      // inject: false
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.s?css/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
