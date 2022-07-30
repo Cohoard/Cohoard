eaff9838f00333589c721112d04770f387c6d531
dzidupe <ekdzidupe@gmail.com>
2022-07-29 22:54:16 -0400

    Created unit and comment models. Set up and added connection to mongo
    Co-authored-by: Samee samee.vohra@gmail.com
    Co-authored-by: Mimi kawaiiyummy141@gmail.com
    Co-authored-by: Jeff JeffreyJWentworth@gmail.com

--- server/server.js
+++ server/server.js
@@ -3,6 +3,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const PORT = 3000;
+const MONGO_URI = 'mongodb+srv://cohoardDB:Vcb7gPGN3mExAiKs@cluster0.lrrzt.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', () => {
+  console.log('Connected to the database');
+})
+
 
 app.use(express.json());
 
