ced9b364ba0549a310142eb11f87a0fa8aac33ac
My Le <mimile@Mys-MacBook-Pro.local>
2022-07-30 09:06:16 -0600

    styling

--- client/App.jsx
+++ client/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 
 function App() {
   return (
-    <div>
-    {/* <main> */}
-      <h1>Sign in to Cohoard</h1>
-    {/* </main> */}
-</div>
+      <>
+    <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">Cohoard</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#dashBoard">Dashboard</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Resources</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   )
 }
 
